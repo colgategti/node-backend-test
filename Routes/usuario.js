663a7552d4b3849f0cfbe91dff20bd449cb2c958
Douglas Alvarenga <colgate@DESKTOP-O0139O4.localdomain>
2021-05-26 13:55:58 -0300

    Creation of routes GET, POST, PUT and DELETE

--- Routes/usuario.js
+++ Routes/usuario.js
@@ -7,4 +7,22 @@ router.get('/', (req, res, next) => {
     })
 })
 
+router.post('/', (req, res, next) => {
+    res.send({
+        mensagem: 'uso do POST'
+    })
+})
+
+router.put('/', (req, res, next) =>{
+    res.send({
+        mensagem:'uso do PUT'
+    })
+})
+
+router.delete('/', (req, res, next)=>{
+    res.send({
+        mensagem:'uso do DELETE'
+    })
+})
+
 module.exports = router;
