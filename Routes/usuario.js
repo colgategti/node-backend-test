0821740f8b4b1e9706677edc5bd9e88e2f7aa4ce
Douglas Alvarenga <colgate@DESKTOP-O0139O4.localdomain>
2021-06-01 19:33:59 -0300

    Addition of Database connection and GET ALL functionality

--- Routes/usuario.js
+++ Routes/usuario.js
@@ -1,25 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+const currentDate = new Date();
 
-router.get('/', (req, res, next) => {
-    res.send({
-        mensagem: 'uso do GET'
-    })
+
+// Conexão do db
+
+var connection = mysql.createConnection({
+    host: "172.17.0.2",
+    user: "root",
+    password: "password",
+    database: "node_test",
+    port: "3306"
 })
 
-router.post('/', (req, res, next) => {
+connection.connect((err)=>{
+    if (err){
+        throw err
+    } else{
+        console.log("connected")
+    }
+})
+
+
+// Acesso da rota GET all em http://localhost:3000/api/usuario
+
+router.get('/usuario', (req, res) => {
+    connection.query(
+        'SELECT * FROM usuarios;',
+        (error, result)=>{
+            if (error) {
+                return res.status(500).send({error: error})
+            }
+            else {
+                console.log('Usuários encontrados com sucesso!')
+                return res.status(200).send({response: result})
+            }
+        })
+})
+
+router.post('/usuario', (req, res, next) => {
     res.send({
         mensagem: 'uso do POST'
     })
 })
 
-router.put('/', (req, res, next) =>{
+router.put('/usuario/:id_usuario', (req, res, next) =>{
     res.send({
         mensagem:'uso do PUT'
     })
 })
 
-router.delete('/', (req, res, next)=>{
+router.delete('/usuario/:id_usuario', (req, res, next)=>{
     res.send({
         mensagem:'uso do DELETE'
     })
